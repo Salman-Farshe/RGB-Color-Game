a08fb8ec02fb4eea4efe44a75d20793efdc09ff1
Salman Farshe <salmanfarshe39@gmail.com>
2021-01-26 22:15:13 +0600

    initialize RGB color game

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let colors = [
+    'rgb(225, 0, 0)',
+    'rgb(155, 255, 0)',
+    'rgb(10, 125, 56)',
+    'rgb(100, 0, 255)',
+    'rgb(250, 59, 200)',
+    'rgb(0, 150, 150)'
+];
+
+let square = document.querySelectorAll(".square");
+
+// picked initial color for displaying in the heading
+let pickedColor = colors[3];
+let displayColor = document.getElementById("displayColor");
+
+displayColor.innerHTML = pickedColor.toUpperCase();
+
+// viewing correct or not
+let displayMessage = document.querySelector(".message");
+
+// change to the heading color
+let headColor = document.querySelector("h1");
+
+for(let i = 0; i < square.length; i++){
+    // add initial color to square
+    square[i].style.backgroundColor = colors[i];
+
+    // add click listener to square
+    square[i].addEventListener("click", function(){
+        // grab the color of clicked square
+        let clickedColor = this.style.backgroundColor;
+        // compare the color between click square and pickedColor
+        if(clickedColor === pickedColor){
+            // if matches, changes background color to all square
+            for(let i = 0; i < square.length; i++){
+                square[i].style.backgroundColor = clickedColor;
+            }
+            displayMessage.textContent = 'Correct !!!';
+            headColor.style.backgroundColor = pickedColor;
+        }
+        else{
+            // color of the body, from css
+            this.style.backgroundColor = "#3a3535";
+            displayMessage.textContent = 'Try Again !!!';
+        }        
+    });
+}
